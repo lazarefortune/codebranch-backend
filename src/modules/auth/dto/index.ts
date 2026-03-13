df75c793bcd7f4fa3e7303dfef72f1fc3018118e
Lazare Fortune <lazarefortune@gmail.com>
2026-03-13 17:34:17 +0100

    reset project

--- src/modules/auth/dto/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './register.dto';
-export * from './login.dto';
-export * from './verify-email.dto';
-export * from './resend-code.dto';
-export * from './forgot-password.dto';
-export * from './reset-password.dto';
--- src/modules/pages/dto/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './create-page.dto';
-export * from './pagination-query.dto';
-export * from './update-username.dto';
-export * from './block.dto';
